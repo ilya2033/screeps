cea128fe8d87f6eb76883ac61a688328d99b79b8
ilya_shyian <ilya.shyian@gmail.com>
2022-11-27 07:37:19 +0200

    - build

--- build/functions/runAllFC.js
+++
@@ -1,6 +0,0 @@
-import { creepFC } from "./creepFC";
-import { roomPositionFC } from "./roomPositionFC";
-export const runAllFC = () => {
-    creepFC();
-    roomPositionFC();
-};
--- build/scripts/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.creepsSpawnScript = void 0;
-const creeps_spawn_1 = __importDefault(require("./creeps.spawn"));
-exports.creepsSpawnScript = creeps_spawn_1.default;
